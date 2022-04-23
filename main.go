d8e8761aa3e4afac93f49aaedb76da30f85aeead
Калашников Владимир Витальевич <pl8415@C201N11.pl1.edu>
2022-04-23 15:33:33 +0500

    sugoma

--- main.go
+++ main.go
@@ -1,40 +1,36 @@
 package main
 
-import(
-	"fmt"
-	"log"
+import (
+	"notice/controllers"
+
 	"github.com/gin-gonic/gin"
-	"github.com/go-pg/pg/v10"
 )
 
-func pgDataBase() (con *pg.DB) {
-	adress := fmt.Srintf("%s:%s", "localhost", "5432")
-	options := &pg.Options{
-		User: "posgress",
-		Password: "",
-		Addr: adress,
-		Database: "notice",
-		PoolSize: 50,
-	}
 
-	con = pg.Connect(options)
-	if con == nil {
-		log.Fatalf("Not today, man")
-	}
-	return
-}
 
 func Api(c *gin.Context) {
-	db := pgDatabase()
-	db.Close()
-
 	c.JSON(200, gin.H{
 		"api": "notice",
 	})
 }
 
+
+
 func main() {
 	r := gin.Default()
 	r.GET("/api", Api)
+
+	rApi := r.Group("/api")
+	{
+		rApi.GET("/notes", controller.GetNotes)
+		rNote := rApi.Group("/note")
+		{
+			rNote.POST("/add", controller.AddNote)
+			rNote.GET("/:id", controller.GetNote)
+			rNote.PUT("/edit", controller.EditNote)
+			rNote.DELETE("/:id", controller.DelNote)
+		}
+	}
+
 	r.Run("0.0.0.0:9090")
 }
