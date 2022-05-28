1b5b4895bf82fdd85df515e11c641a48039648af
Калашников Владимир Витальевич <pl8415@C201N11.pl1.edu>
2022-05-28 14:05:24 +0500

    del

--- ui/src/App.js
+++ ui/src/App.js
@@ -35,6 +35,30 @@ function App() {
       });
   }
 
+  const editNote = (id) => {
+    axios.post(
+      'http://localhost:9090/api/note/edit' + id, 
+      {
+        title: inputTitle.current.value,
+        info: inputInfo.current.value
+      }, 
+      {
+        withCredentials: false
+      }).then(() => {
+        setIsUpdate(!isUpdate);
+      });
+  }
+
+  const deleteNote = (id) => {
+    axios.delete(
+      'http://localhost:9090/api/note/' + id , 
+      {
+        withCredentials: false
+      }).then(() => {
+        setIsUpdate(!isUpdate);
+      });
+  }
+
 
   return (
     <div className="App">
@@ -43,20 +67,36 @@ function App() {
       </header>
       <div className="Main">
         <div className="Container">
-          <label>Заголовок</label>
-          <input ref={inputTitle} type="text"/>
-          <label>Описание</label>
-          <input ref={inputInfo} type="text"/>
-          <button 
-            onClick={() => addNote()}>
-              Добавить
-          </button>
+          <div className="Add">
+            <div>
+              <label>Заголовок</label>
+              <input ref={inputTitle} type="text"/>
+            </div>
+            <div>
+              <label>Описание</label>
+              <input ref={inputInfo} type="text"/>
+            </div>
+            <button 
+              onClick={() => addNote()}>
+                Добавить
+            </button>
+          </div>
 
           <div className="Notes">
             {!!notes && notes.map((note, index) => (
               <div className="Note_block" key={index}>
-                <div>{note.title}</div>
-                <div>{note.info}</div>
+                <div className="Block_text">
+                  <div className="Block_title">{note.title}</div>
+                  <div className="Block_info">{note.info}</div>
+                </div>
+                <div className="Block_buttons">
+                  <button className="Edit_button"
+                    onClick={() => editNote(note.id)}
+                  >Edit</button>
+                  <button className="Delete_button"
+                    onClick={() => deleteNote(note.id)}
+                  >Delete</button>
+                </div>
               </div>
             ))}
           </div>
